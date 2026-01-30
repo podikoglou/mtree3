74fd3d70b7ac23ad131ef9b2a48ab75ed29c32ff
Alex <alex.podikoglou@gmail.com>
2026-01-30 16:30:05 +0200

    feat: keyword parse & tests

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ pub struct Entry {
     pub path: String,
 }
 
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Keyword {
     Type(Type),
     Uid(u32),
@@ -58,6 +59,47 @@ pub fn parse_timestamp<'src>() -> impl Parser<'src, &'src str, DateTime<Utc>> {
         .try_map(|(secs, nsecs), _| {
             DateTime::from_timestamp(secs, nsecs).ok_or(EmptyErr::default())
         })
+}
+
+pub fn parse_keyword<'src>() -> impl Parser<'src, &'src str, Keyword> {
+    let type_value = parse_type();
+
+    let number_u32 = text::int::<_, extra::Err<EmptyErr>>(10)
+        .to_slice()
+        .try_map(|s: &str, _| s.parse::<u32>().map_err(|_| EmptyErr::default()));
+
+    let number_u64 = text::int::<_, extra::Err<EmptyErr>>(10)
+        .to_slice()
+        .try_map(|s: &str, _| s.parse::<u64>().map_err(|_| EmptyErr::default()));
+
+    let timestamp = parse_timestamp();
+
+    choice((
+        just("type")
+            .ignore_then(just("="))
+            .ignore_then(type_value)
+            .map(|ty| Keyword::Type(ty)),
+        just("uid")
+            .ignore_then(just("="))
+            .ignore_then(number_u32)
+            .map(|uid| Keyword::Uid(uid)),
+        just("time")
+            .ignore_then(just("="))
+            .ignore_then(timestamp)
+            .map(|time| Keyword::Time(time)),
+        just("size")
+            .ignore_then(just("="))
+            .ignore_then(number_u64)
+            .map(|size| Keyword::Size(size)),
+        just("sha256")
+            .ignore_then(just("="))
+            .ignore_then(text::ident()) // <-- this may not be right
+            .map(|sha256: &str| Keyword::Sha256(sha256.to_string())),
+        just("link")
+            .ignore_then(just("="))
+            .ignore_then(text::ident()) // <-- this is likely not right
+            .map(|sha256: &str| Keyword::Link(sha256.to_string())),
+    ))
 }
 
 #[cfg(test)]
@@ -132,6 +174,123 @@ fn parse<'a>() -> impl Parser<'a, &'a str, Vec<Entry>> {
     ));
 
     let keywords = keyword.padded();
+    #[test]
+    fn test_parse_type_keyword() {
+        assert_eq!(
+            parse_keyword().parse("type=block").into_result(),
+            Ok(Keyword::Type(Type::Block))
+        );
+        assert_eq!(
+            parse_keyword().parse("type=char").into_result(),
+            Ok(Keyword::Type(Type::Char))
+        );
+        assert_eq!(
+            parse_keyword().parse("type=dir").into_result(),
+            Ok(Keyword::Type(Type::Dir))
+        );
+        assert_eq!(
+            parse_keyword().parse("type=fifo").into_result(),
+            Ok(Keyword::Type(Type::Fifo))
+        );
+        assert_eq!(
+            parse_keyword().parse("type=file").into_result(),
+            Ok(Keyword::Type(Type::File))
+        );
+        assert_eq!(
+            parse_keyword().parse("type=link").into_result(),
+            Ok(Keyword::Type(Type::Link))
+        );
+        assert_eq!(
+            parse_keyword().parse("type=socket").into_result(),
+            Ok(Keyword::Type(Type::Socket))
+        );
+    }
 
-    let command = just('/').then(whitespace()).then(keywords);
+    #[test]
+    fn test_parse_uid_keyword() {
+        assert_eq!(
+            parse_keyword().parse("uid=0").into_result(),
+            Ok(Keyword::Uid(0))
+        );
+        assert_eq!(
+            parse_keyword().parse("uid=100").into_result(),
+            Ok(Keyword::Uid(100))
+        );
+        assert_eq!(
+            parse_keyword().parse("uid=123456789").into_result(),
+            Ok(Keyword::Uid(123456789))
+        );
+    }
+
+    #[test]
+    fn test_parse_timestamp_keyword() {
+        assert_eq!(
+            parse_keyword().parse("time=1630456800.0").into_result(),
+            Ok(Keyword::Time(
+                DateTime::from_timestamp(1630456800, 0).unwrap()
+            ))
+        );
+        assert_eq!(
+            parse_keyword()
+                .parse("time=1769640177.434772208")
+                .into_result(),
+            Ok(Keyword::Time(
+                DateTime::from_timestamp(1769640177, 434772208).unwrap()
+            ))
+        );
+    }
+
+    #[test]
+    fn test_parse_size_keyword() {
+        assert_eq!(
+            parse_keyword().parse("size=0").into_result(),
+            Ok(Keyword::Size(0))
+        );
+        assert_eq!(
+            parse_keyword().parse("size=1024").into_result(),
+            Ok(Keyword::Size(1024))
+        );
+        assert_eq!(
+            parse_keyword().parse("size=1048576").into_result(),
+            Ok(Keyword::Size(1048576))
+        );
+    }
+
+    #[test]
+    fn test_parse_sha256_keyword() {
+        assert_eq!(
+            parse_keyword()
+                .parse("sha256=fd9849d9364b9b9aabed88a8aa8e007d7450c3ad3a17aee0617dd24959464249")
+                .into_result(),
+            Ok(Keyword::Sha256(
+                "fd9849d9364b9b9aabed88a8aa8e007d7450c3ad3a17aee0617dd24959464249".to_string()
+            ))
+        );
+        assert_eq!(
+            parse_keyword()
+                .parse(
+                    "sha256digest=fd9849d9364b9b9aabed88a8aa8e007d7450c3ad3a17aee0617dd24959464249"
+                )
+                .into_result(),
+            Ok(Keyword::Sha256(
+                "fd9849d9364b9b9aabed88a8aa8e007d7450c3ad3a17aee0617dd24959464249".to_string()
+            ))
+        );
+    }
+
+    #[test]
+    fn test_parse_link_keyword() {
+        assert_eq!(
+            parse_keyword().parse("link=../../foo.bar").into_result(),
+            Ok(Keyword::Link("../../foo.bar".to_string()))
+        );
+        assert_eq!(
+            parse_keyword().parse("link=./foo.bar").into_result(),
+            Ok(Keyword::Link("./foo.bar".to_string()))
+        );
+        assert_eq!(
+            parse_keyword().parse("link=foo.bar").into_result(),
+            Ok(Keyword::Link("foo.bar".to_string()))
+        );
+    }
 }
