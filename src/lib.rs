be19af53311ddde5ac9f537525922c96ef603417
Alex <alex.podikoglou@gmail.com>
2026-01-30 16:44:03 +0200

    refactor: get rid of legacy code

--- src/lib.rs
+++ src/lib.rs
@@ -130,50 +130,7 @@ mod tests {
             Ok(DateTime::from_timestamp(1769640177, 434772208).unwrap())
         );
     }
-}
-// fn parse_keyword<'a>() -> impl Parser<'a, &'a str, Keyword> {
 
-// }
-
-fn parse<'a>() -> impl Parser<'a, &'a str, Vec<Entry>> {
-    let type_value = choice((
-        just("block").map(|_| Type::Block),
-        just("char").map(|_| Type::Char),
-        just("dir").map(|_| Type::Dir),
-        just("fifo").map(|_| Type::Fifo),
-        just("file").map(|_| Type::File),
-        just("link").map(|_| Type::Link),
-        just("socket").map(|_| Type::Socket),
-    ));
-
-    let keyword = choice((
-        just("type")
-            .ignore_then(just("="))
-            .ignore_then(type_value)
-            .map(|r#type| Keyword::Type(r#type)),
-        just("uid")
-            .ignore_then(just("="))
-            .ignore_then(text::digits(10).to_slice())
-            .map(|uid| Keyword::Uid(uid)),
-        just("time")
-            .ignore_then(just("="))
-            .ignore_then(text::digits(10).to_slice())
-            .map(|time| Keyword::Time(time)),
-        just("size")
-            .ignore_then(just("="))
-            .ignore_then(text::digits(10).to_slice())
-            .map(|size| Keyword::Size(size)),
-        just("sha256")
-            .ignore_then(just("="))
-            .ignore_then(text::digits(10).to_slice())
-            .map(|sha256| Keyword::Sha256(sha256)),
-        just("link")
-            .ignore_then(just("="))
-            .ignore_then(text::digits(10).to_slice())
-            .map(|link| Keyword::Link(link)),
-    ));
-
-    let keywords = keyword.padded();
     #[test]
     fn test_parse_type_keyword() {
         assert_eq!(
